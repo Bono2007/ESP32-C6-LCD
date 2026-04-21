1cd5a9a2d195e456d87efe26aa57ff2d767a07d1
llc <llc@ac-lille.fr>
2026-04-21 14:49:00 +0200

    fix: retirer include nvs_flash.h inutilise dans wifi_manager

--- main/wifi_manager.c
+++ main/wifi_manager.c
@@ -1,7 +1,6 @@
 #include "wifi_manager.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
-#include "nvs_flash.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
